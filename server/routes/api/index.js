a4bb5e90a89b5a72bba35ec2093bd15bcc9911d7
NikoMayz <mikeayoungjr@gmail.com>
2024-08-08 20:25:40 -0400

    updated bidRoutes files and bidController file

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const bidRoutes = require('./bidRoutes')
 
 router.use('/users', userRoutes);
+router.use('/bids', bidRoutes);
+
 
 module.exports = router;
