f734f96bfb53d4466b0c3ed4b6603c17b1271157
NikoMayz <mikeayoungjr@gmail.com>
2024-08-13 22:01:45 -0400

    created seed.js artwork.json and cleanDB.js changed artwork model to currentHighestBid instead in currentBid

---
+++ server/seeds/seed.js
@@ -0,0 +1,13 @@
+const db = require('../config/connection');
+const Artwork = require('../models/Artwork'); // Import Artwork model
+const cleanDB = require('./utils/cleanDB'); // Import cleanDB utility
+const artworkData = require('.artwork.json'); // Import artwork data from JSON file
+
+db.once('open', async () => {
+  await cleanDB('Artwork', 'artworks'); // Clear the artworks collection
+
+  await Artwork.insertMany(artworkData); // Insert the seed data
+
+  console.log('Artworks seeded!');
+  process.exit(0); // Exit the process after seeding
+});
